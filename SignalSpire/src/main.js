aeaebe9f37ac4b31942c3e8ba0a671612fd79cfe
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2025-12-25 13:36:24 +0530

    Implement basic Three.js setup with a cube in the SignalSpire project

--- SignalSpire/src/main.js
+++ SignalSpire/src/main.js
@@ -1,1 +1,24 @@
-console.log("Hello")
+import * as THREE from 'three'
+
+const sceen = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
+
+const canvas = document.getElementById('draw')
+
+const renderer = new THREE.WebGLRenderer({canvas,antialias:true})
+renderer.setSize(window.innerWidth,window.innerHeight)
+
+
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshStandardMaterial({color:'orange'})
+const cube = new THREE.Mesh(geometry,material);
+
+sceen.add(cube)
+
+
+function action(){
+    renderer.render(sceen,camera)
+    requestAnimationFrame(action)
+}
+action()
